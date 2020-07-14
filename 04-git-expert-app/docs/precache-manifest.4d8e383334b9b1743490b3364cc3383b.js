0494e625f8773fe5d4504a1c271bab8aee144a63
elm0n0 <juanjo.perez.plaza@gmail.com>
2020-07-14 20:56:15 +0200

    cambio a docs

---
+++ 04-git-expert-app/docs/precache-manifest.4d8e383334b9b1743490b3364cc3383b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "946fa713d976916704b054b5b4988202",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa8b8dff69c1473835ff",
+    "url": "/static/css/main.d29d92c1.chunk.css"
+  },
+  {
+    "revision": "1dc87152d4c3d440602e",
+    "url": "/static/js/2.46a8fe2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.46a8fe2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa8b8dff69c1473835ff",
+    "url": "/static/js/main.d012c418.chunk.js"
+  },
+  {
+    "revision": "f1c04d0ff1c1e1d1be53",
+    "url": "/static/js/runtime-main.449490d0.js"
+  }
+]);
